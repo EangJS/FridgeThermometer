508db1e8a59a2b905c4e30183fc7ad87caef7531
EangJS <eugeneang654871@gmail.com>
2023-03-10 00:09:50 +0800

    Add inline keyboard

--- ESP32/fridge_v0.2.ino
+++ ESP32/fridge_v0.2.ino
@@ -22,6 +22,7 @@ int botRequestDelay = 1000;
 unsigned long lastTimeBotRan;
 // Pass our oneWire reference to Dallas Temperature sensor
 DallasTemperature sensors(&oneWire);
+String keyboardJson = "[[{ \"text\" : \"Refresh\", \"callback_data\" : \"0001\" }]]";
 
 void setup() {
   // Start the Serial Monitor
@@ -43,17 +44,28 @@ void handleNewMessages(int numNewMessages) {
   for (int i = 0; i < numNewMessages; i++) {
     String chat_id = String(bot.messages[i].chat_id);
     String text = bot.messages[i].text;
+    int message_id = bot.messages[i].message_id;
     Serial.println(text);
     String from_name = bot.messages[i].from_name;
-    if(text == "/start"){
-        bot.sendMessage(chat_id,"Send me /temp to get the fridge temperature!");
+    if (bot.messages[i].type == "callback_query")
+    {
+      String button_data = bot.messages[i].text;
+      if(button_data == "0001"){
+        sensors.requestTemperatures();
+        float temperatureC = sensors.getTempCByIndex(0);
+        String message = "Hello!, " + from_name + " Temperature is: " + String(temperatureC) + "ºC .\n";
+        bot.sendMessageWithInlineKeyboard(chat_id, message, "", keyboardJson, message_id);
+      }
     }
-    else if (text == "/temp") {
+    else if(text == "/start"){
+        bot.sendMessage(chat_id,"Send me /temp to get the fridge temperature!");
+    } else if (text == "/temp"){
       sensors.requestTemperatures();
       float temperatureC = sensors.getTempCByIndex(0);
-      String welcome = "Hello!, " + from_name + " Temperature is: " + String(temperatureC) + "ºC .\n";
-      bot.sendMessage(chat_id, welcome, "");
-    } else {
+      String message = "Hello!, " + from_name + " Temperature is: " + String(temperatureC) + "ºC .\n";
+      bot.sendMessageWithInlineKeyboard(chat_id, message,"", keyboardJson);
+    }
+    else {
         bot.sendMessage(chat_id,"Unknown input!");
     }
   }
@@ -65,12 +77,16 @@ void checkAlert(float temperatureC){
       Serial.println("High");
       bot.sendMessage(GROUP_ID, message, "");
       delay(10000);
+      sensors.requestTemperatures();
+      temperatureC = sensors.getTempCByIndex(0);
     }
     while(temperatureC < 2.0){
       String message = "Warning Low temperature "  + String(temperatureC) + "ºC .\n";
       Serial.println("Low");
       bot.sendMessage(GROUP_ID, message, "");
       delay(10000);
+      sensors.requestTemperatures();
+      temperatureC = sensors.getTempCByIndex(0);
     }
 }
 
